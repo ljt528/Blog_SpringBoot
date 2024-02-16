4b1a3e07bd6c97cb519b4080a9fa895c23c51a01
ljt528 <ljt0528@gmail.com>
2024-02-17 03:57:11 +0900

    Feat: 공통 객체 타입 선언

---
+++ board-back/src/main/java/com/ljt528/boardback/dto/object/CommentListItem.java
@@ -0,0 +1,17 @@
+package com.ljt528.boardback.dto.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentListItem {
+
+    private String nickname;
+    private String profileImage;
+    private String writeDatetime;
+    private String content;
+    
+}
---
+++ board-back/src/main/java/com/ljt528/boardback/dto/object/FavoriteListItem.java
@@ -0,0 +1,16 @@
+package com.ljt528.boardback.dto.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteListItem {
+
+    private String email;
+    private String nickname;
+    private String profileImage;
+    
+}
