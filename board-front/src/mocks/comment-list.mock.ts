95ee67ebf40c72f9553d6fa7d7751913c2fea331
ljt528 <ljt528@naver.com>
2024-02-18 23:48:18 +0900

    Test: 컴포넌트(댓글 리스트 아이템) 테스트

---
+++ board-front/src/mocks/comment-list.mock.ts
@@ -0,0 +1,24 @@
+import { CommentListItem } from "types/interface";
+
+const commentListMock: CommentListItem[] = [
+    {
+        "nickname": "ljt528",
+        "profileImage": null,
+        "writeDatetime": "1분 전",
+        "content": "내요오오오오오옹내요오오오오오옹내요오오오오오옹내요오오오오오옹내요오오오오오옹내요오오오오오옹"
+    },
+    {
+        "nickname": "ljt528",
+        "profileImage": null,
+        "writeDatetime": "30분 전",
+        "content": "내요오오오오오옹내요오오오오오옹내요오오오오오옹내요오오오오오옹내요오오오오오옹내요오오오오오옹"
+    },
+    {
+        "nickname": "ljt528",
+        "profileImage": null,
+        "writeDatetime": "1시간 전",
+        "content": "내요오오오오오옹내요오오오오오옹내요오오오오오옹내요오오오오오옹내요오오오오오옹내요오오오오오옹"
+    }
+];
+
+export default commentListMock;
