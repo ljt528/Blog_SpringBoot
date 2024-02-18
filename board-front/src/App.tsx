2d738c5f03876e95db866a32d5310b5225b97b69
ljt528 <ljt528@naver.com>
2024-02-19 01:03:22 +0900

    Test: 컴포넌트(InputBox) 테스트

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
 import BoardItem from 'components/BoardItem';
 import Top3Item from 'components/Top3Item';
 import CommentItem from 'components/CommentItem';
 import FavoriteItem from 'components/FavoriteItem';
+import InputBox from 'components/InputBox';
 
 function App() {
+
+  const [value, setValue] = useState<string>('');
   return (
     <>
-    <div style={{ display: 'flex', columnGap: '30px', rowGap: '20px' }}>
-      {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
-    </div>
+      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요.' value={value} error={true} setValue={setValue} message='aaaa' />
     </>
   );
 }
