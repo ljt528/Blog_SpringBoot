29acb47ada45a1b0a08dd22207e245716efbe579
ljt528 <ljt0528@gmail.com>
2024-02-18 23:56:54 +0900

    Test: 컴포넌트(좋아요 리스트 아이템) 테스트

---
+++ board-front/src/mocks/favorite-list.mock.ts
@@ -0,0 +1,34 @@
+const favoriteListMock = [
+    {
+        "email": "email@email.com",
+        "nickname": "ljt528",
+        "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "ljt528",
+        "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "ljt528",
+        "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "ljt528",
+        "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "ljt528",
+        "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "ljt528",
+        "profileImage": null
+    }
+];
+
+export default favoriteListMock;
