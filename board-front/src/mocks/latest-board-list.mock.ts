0a9882fe0adc82b2b61d163bdd755502334b8a99
ljt528 <ljt0528@gmail.com>
2024-02-18 23:03:07 +0900

    Test: 컴포넌트(게시물 리스트 아이템) 테스트

---
+++ board-front/src/mocks/latest-board-list.mock.ts
@@ -0,0 +1,66 @@
+import { BoardListItem } from 'types/interface';
+
+const latestBoardListMock: BoardListItem[] = [
+    {
+        "boardNumber": 1,
+		"title": "하늘을 봐",
+		"content": "하얗게 눈이 내려와 하얀 함박눈이 나비가 날아다니 듯 하얀 눈꽃송이 세상이 온통 하얗게 옷을 갈아입고 있어",
+		"boardTitleImage": 'https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/xnz/image/6rpUwkBxlvL0a8cThCh9vdBreis.jpg',
+		"favoriteCount": 0,
+		"commentCount": 0,
+		"viewCount": 0,
+		"writeDatetime": "2024.01.01. 01:23:45",
+		"writerNickname": "ljt528",
+		"writerProfileImage": 'https://news.kbs.co.kr/data/news/2016/02/16/3232917_8vS.jpg',
+    },
+    {
+        "boardNumber": 2,
+		"title": "저 빛을 따라가",
+		"content": "혜성이 되어 저 하늘을 날아봐 내 맘을 전하게 그대에게 데려가 별을 내려봐 그대가 보이게 더 빛을 태워봐 언젠가 사라져버린다 해도 내 맘을 줄거야",
+		"boardTitleImage": null,
+		"favoriteCount": 0,
+		"commentCount": 0,
+		"viewCount": 0,
+		"writeDatetime": "2024.01.01. 01:23:45",
+		"writerNickname": "ljt528",
+		"writerProfileImage": null,
+    },
+    {
+        "boardNumber": 3,
+		"title": "하늘을 봐",
+		"content": "하얗게 눈이 내려와 하얀 함박눈이 나비가 날아다니 듯 하얀 눈꽃송이 세상이 온통 하얗게 옷을 갈아입고 있어",
+		"boardTitleImage": null,
+		"favoriteCount": 0,
+		"commentCount": 0,
+		"viewCount": 0,
+		"writeDatetime": "2024.01.01. 01:23:45",
+		"writerNickname": "ljt528",
+		"writerProfileImage": null,
+    },
+    {
+        "boardNumber": 4,
+		"title": "하늘을 봐",
+		"content": "하얗게 눈이 내려와 하얀 함박눈이 나비가 날아다니 듯 하얀 눈꽃송이 세상이 온통 하얗게 옷을 갈아입고 있어",
+		"boardTitleImage": null,
+		"favoriteCount": 0,
+		"commentCount": 0,
+		"viewCount": 0,
+		"writeDatetime": "2024.01.01. 01:23:45",
+		"writerNickname": "ljt528",
+		"writerProfileImage": null,
+    },
+    {
+        "boardNumber": 5,
+		"title": "하늘을 봐",
+		"content": "하얗게 눈이 내려와 하얀 함박눈이 나비가 날아다니 듯 하얀 눈꽃송이 세상이 온통 하얗게 옷을 갈아입고 있어",
+		"boardTitleImage": null,
+		"favoriteCount": 0,
+		"commentCount": 0,
+		"viewCount": 0,
+		"writeDatetime": "2024.01.01. 01:23:45",
+		"writerNickname": "ljt528",
+		"writerProfileImage": null,
+    }
+];
+
+export default latestBoardListMock;
