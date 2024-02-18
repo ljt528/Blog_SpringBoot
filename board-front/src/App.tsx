713afafb8f7ca79cb95cb3b6c6c4f0566e6c3a40
ljt528 <ljt0528@gmail.com>
2024-02-19 01:22:17 +0900

    Docs: Footer 레이아웃 작성

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,18 +1,11 @@
-import React, { useState } from 'react';
+import Footer from 'layouts/Footer';
 import './App.css';
-import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
-import BoardItem from 'components/BoardItem';
-import Top3Item from 'components/Top3Item';
-import CommentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
 
 function App() {
 
-  const [value, setValue] = useState<string>('');
   return (
     <>
-      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요.' value={value} error={true} setValue={setValue} message='aaaa' />
+      <Footer />
     </>
   );
 }
