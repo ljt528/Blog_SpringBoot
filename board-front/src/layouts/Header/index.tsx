e4857475116cba015a9d41c16b843368911acdae
ljt528 <ljt528@naver.com>
2024-02-27 22:36:53 +0900

    Feat: React-Router-Dom SPA 작업

---
+++ board-front/src/layouts/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+
+//          component : Header 레이아웃             //
+export default function Header() {
+
+  //          render : Header 레이아웃 렌더링             //
+  return (
+    <div>header</div>
+  );
+};
