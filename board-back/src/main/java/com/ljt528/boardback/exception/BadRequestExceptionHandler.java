8d729a4d82f73b953d5ee77524d86251955d3c0a
ljt528 <ljt528@naver.com>
2024-03-16 18:48:44 +0900

    Fix: Validation Failed 처리

---
+++ board-back/src/main/java/com/ljt528/boardback/exception/BadRequestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.ljt528.boardback.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.ljt528.boardback.dto.response.ResponseDto;
+
+@RestControllerAdvice
+public class BadRequestExceptionHandler {
+    
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<ResponseDto> validationExceptionHandler(Exception exception) {
+        return ResponseDto.validationFailed();
+    }
+}
