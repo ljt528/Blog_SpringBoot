ac539802a8918268570e842dee409b1834e37686
ljt528 <ljt528@naver.com>
2024-04-09 16:58:25 +0900

    Docs: 게시물 상세 페이지 이벤트 처리

---
+++ board-front/src/mocks/board.mock.ts
@@ -0,0 +1,14 @@
+import { Board } from 'types/interface';
+
+const boardMock: Board = {
+    boardNumber: 1,
+    title: 'Mock Test Title',
+    content: 'Mock Test Content',
+    boardImageList: ['https://cdn.imweb.me/upload/S2021111263c91c14a04fa/f87457db71e8b.png', 'https://www.palnews.co.kr/news/photo/201801/92969_25283_5321.jpg'],
+    writeDatetime: '2024. 04. 09.',
+    writerEmail: 'email@email.com',
+    writerNickname: 'ljt528',
+    writerProfileImage: 'https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg',
+};
+
+export default boardMock;
