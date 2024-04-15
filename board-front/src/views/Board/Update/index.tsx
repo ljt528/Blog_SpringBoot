cd8ff485b72b8182948f6cff903f40c62f8b1b41
ljt528 <ljt528@naver.com>
2024-04-16 01:03:06 +0900

    Docs: 게시물 수정 페이지 API 연동

--- board-front/src/views/Board/Update/index.tsx
+++ board-front/src/views/Board/Update/index.tsx
@@ -42,7 +42,7 @@ export default function BoardWrite() {
 
 
   //        function : 네비게이트 함수        //
-  const navigator = useNavigate();
+  const navigate = useNavigate();
 
   //        function : get board response 처리 함수        //
   const getBoardResponse = (responseBody: GetBoardResponseDto | ResponseDto | null) => {
@@ -51,7 +51,7 @@ export default function BoardWrite() {
       if(code === 'NB') alert('존재하지 않는 게시물입니다.');
       if(code === 'DBE') alert('데이터베이스 오류입니다.');
       if(code !== 'SU') {
-        navigator(MAIN_PATH());
+        navigate(MAIN_PATH());
         return;
       }
 
@@ -62,7 +62,7 @@ export default function BoardWrite() {
       convertUrlsToFile(boardImageList).then(boardImageFileList => setBoardImageFileList(boardImageFileList));
 
       if (!loginUser || loginUser.email !== writerEmail) {
-        navigator(MAIN_PATH());
+        navigate(MAIN_PATH());
         return;
       }
 
@@ -137,7 +137,7 @@ export default function BoardWrite() {
   useEffect(() => {
     const accessToken = cookies.accessToken;
     if (!accessToken) {
-      navigator(MAIN_PATH());
+      navigate(MAIN_PATH());
       return;
     }
     if (!boardNumber) return;
