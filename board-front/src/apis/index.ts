696fd2295f44e7db59abbb75ef4bb59dc35bddc0
ljt528 <ljt528@naver.com>
2024-04-18 00:05:51 +0900

    Docs: 메인 페이지 API 연동

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -4,7 +4,8 @@ import { SignInResponseDto, SignUpResponseDto } from './response/auth';
 import { ResponseDto } from './response';
 import { GetSignInUserResponseDto } from './response/user';
 import { PatchBoardRequestDto, PostBoardRequestDto, PostCommentRequestDto } from './request/board';
-import { PostBoardResponseDto, GetBoardResponseDto, IncreaseViewCountResponseDto, GetFavoriteListResponseDto, GetCommentListResponseDto, PutFavoriteResponseDto, PostCommentResponseDto, DeleteBoardResponseDto, PatchBoardResponseDto } from './response/board';
+import { PostBoardResponseDto, GetBoardResponseDto, IncreaseViewCountResponseDto, GetFavoriteListResponseDto, GetCommentListResponseDto, PutFavoriteResponseDto, PostCommentResponseDto, DeleteBoardResponseDto, PatchBoardResponseDto, GetLatestBoardListResponseDto, GetTop3BoardListResponseDto } from './response/board';
+import { GetPopularListResponseDto } from './response/search';
 
 const DOMAIN = 'http://localhost:4000';
 
@@ -46,6 +47,8 @@ export const signUpRequest = async (requestBody: SignUpRequestDto) => {
 };
 
 const GET_BOARD_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}`;
+const GET_LATEST_BOARD_LIST_URL = () => `${API_DOMAIN}/board/latest-list`;
+const GET_TOP_3_BOARD_LIST_URL = () => `${API_DOMAIN}/board/top-3`;
 const INCREASE_VIEW_COUNT_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/increase-view-count`;
 const GET_FAVORITE_LIST_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/favorite-list`;
 const GET_COMMENT_LIST_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/comment-list`;
@@ -68,6 +71,34 @@ export const getBoardRequest = async (boardNumber: number | string) => {
             return responseBody;
         });
         return result;
+};
+
+export const getLatestBoardListRequest = async () => {
+    const result = await axios.get(GET_LATEST_BOARD_LIST_URL())
+        .then(response => {
+            const responseBody: GetLatestBoardListResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            if (!error.response) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+        });
+    return result;
+};
+
+export const getTop3BoardListRequest = async () => {
+    const result = await axios.get(GET_TOP_3_BOARD_LIST_URL())
+        .then(response => {
+            const responseBody: GetTop3BoardListResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            if (!error.response) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+        });
+    return result;
 };
 
 export const increaseViewCountRequest = async (boardNumber: number | string) => {
@@ -172,6 +203,22 @@ export const deleteBoardRequest = async (boardNumber: number | string, accessTok
     const result = await axios.delete(DELETE_BOARD_URL(boardNumber), authorization(accessToken))
         .then(response => {
             const responseBody: DeleteBoardResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            if (!error.response) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+        });
+    return result;
+};
+
+const GET_POPULAR_LIST_URL = () => `${API_DOMAIN}/search/popular-list`;
+
+export const getPopularListRequest = async () => {
+    const result = await axios.get(GET_POPULAR_LIST_URL())
+        .then(response => {
+            const responseBody: GetPopularListResponseDto = response.data;
             return responseBody;
         })
         .catch(error => {
--- board-front/src/views/Main/index.tsx
+++ board-front/src/views/Main/index.tsx
@@ -7,19 +7,38 @@ import BoardItem from 'components/BoardItem';
 import Pagination from 'components/Pagination';
 import { useNavigate } from 'react-router-dom';
 import { SEARCH_PATH } from 'constant';
+import { getLatestBoardListRequest, getPopularListRequest, getTop3BoardListRequest } from 'apis';
+import { GetLatestBoardListResponseDto, GetTop3BoardListResponseDto } from 'apis/response/board';
+import { ResponseDto } from 'apis/response';
+import { usePagination } from 'hooks';
+import { GetPopularListResponseDto } from 'apis/response/search';
 
 //          component : 메인 화면 컴포넌트            //
 export default function Main() {
 
+  //        function : 네비게이트 함수       //
+  const navigate = useNavigate();
+  
   //          component : 메인 화면 상단 컴포넌트            //
   const MainTop = () => {
 
     //        state : 주간 top3 게시물 리스트 상태        //
     const [top3BoardList, setTop3BoardList] = useState<BoardListItem[]>([]);
 
+    //        function : get top 3 board list response 처리 함수        //
+    const getTop3BoardListResponse = (responseBody: GetTop3BoardListResponseDto | ResponseDto | null) => {
+      if (!responseBody) return;
+      const { code } = responseBody;
+      if (code === 'DBE') alert('데이터베이스 오류입니다.');
+      if (code !== 'SU') return;
+
+      const { top3List } = responseBody as GetTop3BoardListResponseDto;
+      setTop3BoardList(top3List);
+    };
+
     //        effect : 첫 마운트 시 실행될 함수        //
     useEffect(() => {
-      setTop3BoardList(top3BoardListMock);
+      getTop3BoardListRequest().then(getTop3BoardListResponse);
     }, []);
 
     //        render : 메인 화면 상단 컴포넌트 렌더링        //
@@ -42,14 +61,36 @@ export default function Main() {
   //          component : 메인 화면 하단 컴포넌트            //
   const MainBottom = () => {
 
-    //        function : 네비게이트 함수       //
-    const navigate = useNavigate();
-
-    //        state : 최신 게시물 리스트 상태 (임시)       //
-    const [currentBoardList, setCurrentBoardList] = useState<BoardListItem[]>([]);
+    //        state : 페이지네이션 관련 상태       //
+    const { 
+      currentPage, currentSection, viewList, viewPageList, totalSection,
+      setCurrentPage, setCurrentSection, setTotalList
+    } = usePagination<BoardListItem>(5);
 
     //        state : 인기 검색어 리스트 상태       //
     const [popularWordList, setPopularWordList] = useState<string[]>([]);
+
+    //        function : get latest board list response 처리 함수       //
+    const getLatestBoardListResponse = (responseBody: GetLatestBoardListResponseDto | ResponseDto | null) => {
+      if (!responseBody) return;
+      const { code } = responseBody;
+      if (code === 'DBE') alert('데이터베이스 오류입니다.');
+      if (code === 'SU') return;
+
+      const { latestList } = responseBody as GetLatestBoardListResponseDto;
+      setTotalList(latestList);
+    };
+
+    //        function : get popular list response 처리 함수       //
+    const getPopularListResponse = (responseBody: GetPopularListResponseDto | ResponseDto | null) => {
+      if (!responseBody) return;
+      const { code } = responseBody;
+      if (code === 'DBE') alert('데이터베이스 오류입니다.');
+      if (code === 'SU') return;
+
+      const { popularWordList } = responseBody as GetPopularListResponseDto;
+      setPopularWordList(popularWordList);
+    };
 
     //        event handler : 인기 검색어 클릭 이벤트 처리       //
     const onPopularWordClickHandler = (word: string) => {
@@ -58,8 +99,8 @@ export default function Main() {
 
     //        effect : 첫 마운트 시 실행될 함수        //
     useEffect(() => {
-      setCurrentBoardList(latestBoardListMock);
-      setPopularWordList(['Java', 'Python', 'C++']);
+      getLatestBoardListRequest().then(getLatestBoardListResponse);
+      getPopularListRequest().then(getPopularListResponse);
     }, []);
 
     //        render : 메인 화면 하단 컴포넌트 렌더링        //
@@ -69,7 +110,7 @@ export default function Main() {
           <div className='main-bottom-title'>{'최신 게시물'}</div>
           <div className='main-bottom-contents-box'>
             <div className='main-bottom-current-contents'>
-              {currentBoardList.map(boardListItem => <BoardItem boardListItem={boardListItem} />)}
+              {viewList.map(boardListItem => <BoardItem boardListItem={boardListItem} />)}
             </div>
             <div className='main-bottom-popular-box'>
               <div className='main-bottom-popular-card'>
@@ -83,7 +124,14 @@ export default function Main() {
             </div>
           </div>
           <div className='main-bottom-pagination-box'>
-            {/* <Pagination /> */}
+            <Pagination
+              currentPage={currentPage}
+              currentSection={currentSection}
+              setCurrentPage={setCurrentPage}
+              setCurrentSection={setCurrentSection}
+              viewPageList={viewPageList}
+              totalSection={totalSection}
+            />
           </div>
         </div>
       </div>
