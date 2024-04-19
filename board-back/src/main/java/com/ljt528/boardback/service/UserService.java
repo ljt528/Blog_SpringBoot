7a97502e7eddaee64b3d420e961e45fe2178ecc2
ljt528 <ljt528@naver.com>
2024-04-19 21:44:37 +0900

    Feat: 닉네임 및 프로필 이미지 수정 API 작성

--- board-back/src/main/java/com/ljt528/boardback/service/UserService.java
+++ board-back/src/main/java/com/ljt528/boardback/service/UserService.java
@@ -2,11 +2,18 @@ package com.ljt528.boardback.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.ljt528.boardback.dto.request.user.PatchNicknameRequestDto;
+import com.ljt528.boardback.dto.request.user.PatchProfileImageRequestDto;
 import com.ljt528.boardback.dto.response.user.GetSignInUserResponseDto;
 import com.ljt528.boardback.dto.response.user.GetUserResponseDto;
+import com.ljt528.boardback.dto.response.user.PatchNicknameResponseDto;
+import com.ljt528.boardback.dto.response.user.PatchProfileImageResponseDto;
 
 public interface UserService {
 
     ResponseEntity<? super GetUserResponseDto> getUser(String email);
     ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(String email);
+    ResponseEntity<? super PatchNicknameResponseDto> patchNickname(PatchNicknameRequestDto dto, String email);
+    ResponseEntity<? super PatchProfileImageResponseDto> patchProfileImage(PatchProfileImageRequestDto dto, String email);
+
 }
--- board-back/src/main/java/com/ljt528/boardback/service/implement/UserServiceImplement.java
+++ board-back/src/main/java/com/ljt528/boardback/service/implement/UserServiceImplement.java
@@ -3,9 +3,13 @@ package com.ljt528.boardback.service.implement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.ljt528.boardback.dto.request.user.PatchNicknameRequestDto;
+import com.ljt528.boardback.dto.request.user.PatchProfileImageRequestDto;
 import com.ljt528.boardback.dto.response.ResponseDto;
 import com.ljt528.boardback.dto.response.user.GetSignInUserResponseDto;
 import com.ljt528.boardback.dto.response.user.GetUserResponseDto;
+import com.ljt528.boardback.dto.response.user.PatchNicknameResponseDto;
+import com.ljt528.boardback.dto.response.user.PatchProfileImageResponseDto;
 import com.ljt528.boardback.entity.UserEntity;
 import com.ljt528.boardback.repository.UserRepository;
 import com.ljt528.boardback.service.UserService;
@@ -53,6 +57,51 @@ public class UserServiceImplement implements UserService {
         }
 
         return GetSignInUserResponseDto.success(userEntity);
+
+    }
+
+    @Override
+    public ResponseEntity<? super PatchNicknameResponseDto> patchNickname(PatchNicknameRequestDto dto, String email) {
+
+        try {
+            
+            UserEntity userEntity = userRepository.findByEmail(email);
+            if (userEntity == null) PatchNicknameResponseDto.noExistUser();
+
+            String nickname = dto.getNickname();
+            boolean existedNickname = userRepository.existsByNickname(nickname);
+            if (existedNickname) return PatchNicknameResponseDto.duplicateNickname();
+
+            userEntity.setNickname(nickname);
+            userRepository.save(userEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return PatchNicknameResponseDto.success();
+
+    }
+
+    @Override
+    public ResponseEntity<? super PatchProfileImageResponseDto> patchProfileImage(PatchProfileImageRequestDto dto, String email) {
+
+        try {
+            
+            UserEntity userEntity = userRepository.findByEmail(email);
+            if (userEntity == null) return PatchProfileImageResponseDto.noExistUser();
+
+            String profileImage = dto.getProfileImage();
+            userEntity.setProfileImage(profileImage);;
+            userRepository.save(userEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return PatchProfileImageResponseDto.success();
 
     }
 
